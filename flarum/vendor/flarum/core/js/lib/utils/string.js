0ca230b33f3b3a6cc4f53a33e7a29b52c473eb14
ubuntu <ubuntu@ubuntu-virtual-machine.(none)>
2015-10-04 15:46:55 +0700

    Title slug for vietnamese

--- flarum/vendor/flarum/core/js/lib/utils/string.js
+++ flarum/vendor/flarum/core/js/lib/utils/string.js
@@ -13,6 +13,27 @@ export function truncate(string, length, start = 0) {
 }
 
 /**
+ * Create vietnamese slug.
+ * @param {String} string
+ * @return {String}
+ */
+function slugVietnamese(string) {
+  var str = (string || '').toLowerCase();
+  str = str.toLowerCase();
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ  |ặ|ẳ|ẵ/g, "a");
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ  |ợ|ở|ỡ/g, "o");
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+  str = str.replace(/đ/g, "d");
+  str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'| |\"|\&|\#|\[|\]|~|$|_/g, "-");
+  str = str.replace(/-+-/g, "-");
+  str = str.replace(/^\-+|\-+$/g, "");
+  return str;
+}
+
+/**
  * Create a slug out of the given string. Non-alphanumeric characters are
  * converted to hyphens.
  *
@@ -20,7 +41,7 @@ export function truncate(string, length, start = 0) {
  * @return {String}
  */
 export function slug(string) {
-  return string.toLowerCase()
+  return slugVietnamese(string)
     .replace(/[^a-z0-9]/gi, '-')
     .replace(/-+/g, '-')
     .replace(/-$|^-/g, '') || '-';
